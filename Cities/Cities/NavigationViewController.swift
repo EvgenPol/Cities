ee6daab13e6baeee89cfaf6f28d0c050de42c3bf
EvgenPol <78258561+EvgenPol@users.noreply.github.com>
2021-08-07 18:23:23 +0300

    Start

---
+++ Cities/Cities/NavigationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationViewController.swift
+//  Cities
+//
+//  Created by Евгений Полюбин on 07.08.2021.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
